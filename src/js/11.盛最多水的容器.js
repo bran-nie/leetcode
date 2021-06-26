34ceb020ef3c732b74faa78378e98bda409db70d
bran.nie <pengcheng1995@gmail.com>
2021-06-26 15:17:10 +0800

    medium: two-pointers

--- src/js/11.盛最多水的容器.js
+++ src/js/11.盛最多水的容器.js
@@ -9,8 +9,18 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
+    // 双指针
+    // 分类大致有快慢指针、左右指针，这里使用左右指针
+    let left = 0,
+        right = height.length - 1;
+    let result = 0;
 
+    while (left < right) {
+        let cur = Math.min(height[left], height[right]) * (right - left);
+        result = Math.max(result, cur);
+        height[left] > height[right] ? right-- : left++;
+    }
+    return result;
 };
 // @lc code=end
-
