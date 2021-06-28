55c34f6697bbbb077ff1e2c2144aae89acd80881
bran.nie <pengcheng1995@gmail.com>
2021-06-28 15:24:19 +0800

    easy: linklist

---
+++ src/js/19.删除链表的倒数第-n-个结点.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=javascript
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    const tmpHead = new ListNode();
+    tmpHead.next = head;
+    let p = tmpHead,
+        q = tmpHead;
+    while (n--) {
+        q = q.next;
+    }
+    while (q && q.next !== null) {
+        p = p.next;
+        q = q.next;
+    }
+    p.next = p.next.next;
+    return tmpHead.next;
+};
+// @lc code=end
