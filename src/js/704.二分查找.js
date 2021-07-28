095c8a01994dfcb50abf0e146ebbbd61cf005330
bran.nie <pengcheng1995@gmail.com>
2021-07-28 15:37:00 +0800

    easy: 二分查找

---
+++ src/js/704.二分查找.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    if (nums.length === 0) return -1;
+    let left = 0,
+        right = nums.length - 1;
+    while (left <= right) {
+        const mid = ((right - left) >> 1) + left;
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+};
+// @lc code=end
