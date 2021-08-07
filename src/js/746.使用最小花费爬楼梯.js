84bcc5be54152ed6484c3b78f48663e456352527
bran.nie <pengcheng1995@gmail.com>
2021-08-07 10:51:43 +0800

    easy:  动态规划

---
+++ src/js/746.使用最小花费爬楼梯.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=javascript
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+    const n = cost.length;
+
+    let prev = 0,
+        cur = 0;
+    for (let i = 2; i <= n; i++) {
+        let next = Math.min(cur + cost[i - 1], prev + cost[i - 2]);
+
+        prev = cur;
+        cur = next;
+    }
+
+    return cur;
+};
+// @lc code=end
