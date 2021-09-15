7b2757d2ec4b5e86949b4ef6b20fe172233624c9
bran.nie <pengcheng1995@gmail.com>
2021-09-15 10:41:03 +0800

    medium: 动态规划

---
+++ src/js/96.不同的二叉搜索树.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=96 lang=javascript
+ *
+ * [96] 不同的二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+    // F(i,n) = G(i-1) * G(n-i)
+    const dp = Array(n + 1).fill(0);
+    (dp[0] = 1), (dp[1] = 1);
+
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+    }
+    return dp[n];
+};
+// @lc code=end
