4a771b2c006edea5c22aeccbab4455dc3ee73647
bran.nie <pengcheng1995@gmail.com>
2021-09-28 16:41:44 +0800

    medium: 深度优先搜索，前缀和

---
+++ src/js/437.路径总和-iii.js
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=437 lang=javascript
+ *
+ * [437] 路径总和 III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+const dfs = (root, prefix, curr, targetSum) => {
+    if (root === null) {
+        return 0;
+    }
+    let ret = 0;
+    curr += root.val;
+
+    ret = prefix.get(curr - targetSum) || 0;
+    prefix.set(curr, (prefix.get(curr) || 0) + 1);
+    ret += dfs(root.left, prefix, curr, targetSum);
+    ret += dfs(root.right, prefix, curr, targetSum);
+    prefix.set(curr, (prefix.get(curr) || 0) - 1);
+
+    return ret;
+};
+var pathSum = function (root, targetSum) {
+    const prefix = new Map();
+    prefix.set(0, 1);
+    return dfs(root, prefix, 0, targetSum);
+};
+// @lc code=end
+
+var pathSum = function (root, targetSum) {
+    let ans = 0;
+
+    const hashmap = new Map();
+
+    const dfs = (root, sum, isRoot) => {
+        if (root === null) {
+            return;
+        }
+        if (isRoot) {
+            if (hashmap.has(root)) {
+                return;
+            }
+            hashmap.set(root, true);
+        }
+        const v = sum - root.val;
+        // console.log({ val: root.val, sum, v, isRoot })
+        if (v === 0) {
+            // console.log(root.val)
+            ans++;
+        }
+        dfs(root.left, v);
+        dfs(root.left, targetSum, true);
+        dfs(root.right, v);
+        dfs(root.right, targetSum, true);
+    };
+
+    dfs(root, targetSum, true);
+    return ans;
+};
